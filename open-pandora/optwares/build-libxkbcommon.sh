7256400a055efe3873f4ecf7dbb5dc8eb3fcfbd9
Nox P@sNox <pasnox@gmail.com>
2013-05-26 18:35:16 +0200

    Add new optwares (needed for sqfs-mounter / qt build)

---
+++ open-pandora/optwares/build-libxkbcommon.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+. build-constants.sh
+
+if [ ! -d "${1}" ]; then
+    echo "Please give an existing libxkbcommon sources folder as first parameter to this script."
+    exit 1
+fi
+
+CUR_PWD="${PWD}"
+
+cd "${1}"
+
+./configure \
+--host="${CROSS_DEVICE_VENDOR}" \
+--prefix="${CROSS_DEVICE_OPTWARES_PATH}" \
+--enable-shared \
+--enable-static \
+&& \
+make V=1 -j ${CROSS_MAKE_JOBS} \
+&& \
+make install
+
+cd "${CUR_PWD}"
---
+++ open-pandora/optwares/build-qt-5.1.x.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+. build-constants.sh no-export
+
+if [ ! -d "${1}" ]; then
+    echo "Please give the Qt sources folder to build as first parameter."
+    exit 1
+fi
+
+CROSS_DEVICE_MKSPEC="linux-open-pandora-g++"
+QT_VERSION="`basename \"${1}\"`"
+QT_VERSION="`echo \"${QT_VERSION}\" | cut -d'-' -f5`"
+QT_WILDCARD_VERSION="`echo \"${QT_VERSION}\" | cut -d'.' -f1,2`.x"
+
+CUR_PWD="${PWD}"
+
+if [ -L "${1}/qtbase/mkspecs/${CROSS_DEVICE_MKSPEC}" ]; then
+    rm -f "${1}/qtbase/mkspecs/${CROSS_DEVICE_MKSPEC}"
+fi
+
+if [ -d "${1}/qtbase/mkspecs/${CROSS_DEVICE_MKSPEC}" ]; then
+    rm -fr "${1}/qtbase/mkspecs/${CROSS_DEVICE_MKSPEC}"
+fi
+
+cp -r "${PWD}/Qt/mkspecs/${QT_WILDCARD_VERSION}/${CROSS_DEVICE_MKSPEC}" "${1}/qtbase/mkspecs/"
+
+cd "${1}"
+
+# fix dbus test/build
+echo "LIBS *= -ldbus-1" >> "qtbase/config.tests/unix/dbus/dbus.pro"
+echo "INCLUDEPATH *= /usr/include/dbus-1.0 /usr/lib/x86_64-linux-gnu/dbus-1.0/include" >> "qtbase/src/tools/bootstrap-dbus/bootstrap-dbus.pro"
+echo "INCLUDEPATH *= /usr/include/dbus-1.0 /usr/lib/x86_64-linux-gnu/dbus-1.0/include" >> "qtbase/src/tools/qdbuscpp2xml/qdbuscpp2xml.pro"
+echo "INCLUDEPATH *= /usr/include/dbus-1.0 /usr/lib/x86_64-linux-gnu/dbus-1.0/include" >> "qtbase/src/tools/qdbusxml2cpp/qdbusxml2cpp.pro"
+
+# fix xcb / x11 test / build
+echo "LIBS *= -lXau -lXdmcp" >> "qtbase/config.tests/qpa/xcb/xcb.pro"
+echo "LIBS *= -lXau -lXdmcp" >> "qtbase/config.tests/qpa/xcb-xkb/xcb-xkb.pro"
+
+./configure \
+-prefix "${CROSS_DEVICE_OPTWARES_PATH}/../Qt/${QT_VERSION}" \
+-opensource \
+-confirm-license \
+-platform linux-g++-64 \
+-arch arm \
+-xplatform "${CROSS_DEVICE_MKSPEC}" \
+-no-c++11 \
+-no-pkg-config \
+-nomake examples \
+-nomake demos \
+-make tools \
+-no-pch \
+-qt-xkbcommon \
+-qt-xcb \
+-qpa xcb \
+-opengl es2 \
+&& make -j ${CROSS_MAKE_JOBS} \
+&& make install
+
+cd "${CUR_PWD}"
